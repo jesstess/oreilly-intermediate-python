536971c8b3cfb28bf267bd4e2b1599e9e782d7ae
Jessica McKellar <jesstess@mit.edu>
2016-06-04 22:42:50 -0700

    Undo the timing additions to sonnets.py -- that's now how we start.

--- Unit3/sonnets/sonnets.py
+++ Unit3/sonnets/sonnets.py
@@ -1,19 +1,11 @@
-import time
-
 my_words = [elt.strip() for elt in open("sonnet_words.txt", "r").readlines()]
 word_list = [elt.strip() for elt in open("sowpods.txt", "r").readlines()]
-word_set = set(word_list)
 
 counter = 0
 
-start = time.time()
-
 for word in my_words:
-    if word not in word_set:
+    if word not in word_list:
         print(word)
         counter += 1
 
-stop = time.time()
-
 print("Total new words: %d" % counter)
-print("Time elapsed: %.1f seconds" % (stop - start))
